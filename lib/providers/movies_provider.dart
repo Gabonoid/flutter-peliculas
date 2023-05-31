310b6ae4451558807722c93cd6177e04e71919f2
Gabriel Serrano <gabo09link@gmail.com>
2023-05-30 18:16:25 -0600

    optimizando codigo de las peticiones al API

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -8,19 +8,36 @@ class MoviesProvider extends ChangeNotifier {
   final String _baseURL = 'api.themoviedb.org';
   final String _language = 'es-ES';
 
-  List<Movie> onDisplayMovie = [];
+  List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
+
+  int _popularPage = 0;
 
   MoviesProvider() {
-    print('MoviesProvider inicializado');
     getOnDisplayMovie();
+    getPopularMovies();
+  }
+
+  Future<String> _getJsonData(String endpoint, [int page = 1]) async {
+    var url = Uri.https(_baseURL, endpoint,
+        {'api_key': _apikey, 'language': _language, 'page': '$page'});
+
+    final response = await http.get(url);
+    return response.body;
   }
 
   getOnDisplayMovie() async {
-    var url = Uri.https(_baseURL, '3/movie/now_playing',
-        {'api_key': _apikey, 'language': _language, 'page': '1'});
+    final jsonData = await _getJsonData('3/movie/now_playing');
+    final nowPlayingResponse = NowPlayingResponse.fromJson(jsonData);
+    onDisplayMovies = nowPlayingResponse.results;
+    notifyListeners();
+  }
 
-    final response = await http.get(url);
-    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
-    onDisplayMovie = nowPlayingResponse.results;
+  getPopularMovies() async {
+    _popularPage++;
+    final jsonData = await _getJsonData('3/movie/popular', _popularPage);
+    final popularResponse = PopularResponse.fromJson(jsonData);
+    popularMovies = [...popularMovies, ...popularResponse.results];
+    notifyListeners();
   }
 }
